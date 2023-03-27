8f7770772fbd4050648baf28ee901f119a1c9719
강준 <keisoft.kjun@gmail.com>
2023-03-28 00:17:58 +0900

    초기 커밋.

---
+++ ConsoleAppNatsWorker/Program.cs
@@ -0,0 +1,31 @@
+﻿using NATS.Client;
+
+namespace ConsoleAppNatsWorker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Worker");
+
+            ConnectionFactory cf = new ConnectionFactory();
+            Options opts = ConnectionFactory.GetDefaultOptions();
+            opts.Url = "nats://localhost:4222";
+
+            IConnection c = cf.CreateConnection(opts);
+
+            EventHandler<MsgHandlerEventArgs> h = (sender, args) =>
+            {
+                Console.WriteLine($"worker received {args.Message}");
+            };
+
+            IAsyncSubscription s = c.SubscribeAsync("worker", h);
+
+            while (true)
+            {
+                Console.WriteLine("worker listening...");
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
---
+++ WebAppNatsSerer/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+using Microsoft.AspNetCore.Mvc;
+
+using NATS.Client;
+
+namespace WebAppNatsSerer.Controllers
+{
+    [Route("[controller]")]
+    public class ValuesController : Controller
+    {
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            Thread.Sleep(5000);
+
+            ConnectionFactory cf = new ConnectionFactory();
+            Options opts = ConnectionFactory.GetDefaultOptions();
+
+            opts.Url = "nats://localhost:4222";
+
+            IConnection c = cf.CreateConnection(opts);
+
+            c.Publish("worker", Encoding.UTF8.GetBytes($"hello, world: {value}"));
+            c.Close();
+
+            return Ok();
+        }
+    }
+}
